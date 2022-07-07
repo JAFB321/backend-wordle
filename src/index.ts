a7245f132ed6e6914128f78f840f0cbbc28faba1
jafb321 <jafb321@gmail.com>
2022-07-07 13:41:24 -0700

    feat: install dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
 import express, {json} from 'express';
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(json());
 
 app.get('/', (req, res) => {
@@ -9,4 +13,4 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log(`Server listening on port ${3000}`))
+app.listen(port, () => console.log(`Server listening on port ${port}`))
