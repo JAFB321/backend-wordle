cbab4b4397e1bf38e4470c327ddf37dc49b77311
jafb321 <jafb321@gmail.com>
2022-07-08 21:18:44 -0700

    feat: use verify unauth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import express, {json} from 'express';
+import { verifyUnauthRequest } from './middlewares/auth';
 import appRoutes from './routes/index';
 import wordUpdater from './workers/wordUpdater';
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(json());
 app.use('/api', appRoutes);
+app.use(verifyUnauthRequest)
 
 // Update word each 5 minutes
-wordUpdater.init(2);
+wordUpdater.init(360);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
